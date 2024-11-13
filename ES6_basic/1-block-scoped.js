b42974209eadf2ae01f6a51ced2bdb3fe08fdacd
Bomays <9314@holbertonstudents.com>
2024-11-13 09:55:15 +0100

    added const variable in loop and renamed them

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const notoverwrittenTask = true;
+    const notoverwrittenTask2 = false;
   }
 
   return [task, task2];
