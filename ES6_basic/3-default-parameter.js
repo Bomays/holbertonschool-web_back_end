9309a07d94ce9ec8e17a6c45d4e1b808ce949e7e
Bomays <9314@holbertonstudents.com>
2024-11-13 11:12:26 +0100

    added /* eslint-disable */

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,5 +1,2 @@
-export default (
-  initialNumber,
-  expansion1989 = 89,
-  expansion2019 = 19,
-) => initialNumber + expansion1989 + expansion2019;
+/* eslint-disable */
+export default (initialNumber, expansion1989 = 89, expansion2019 = 19) => initialNumber + expansion1989 + expansion2019;
