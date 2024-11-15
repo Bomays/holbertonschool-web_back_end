6e1e53c5645ab6619be5280c1eda301256c4d79c
Bomays <9314@holbertonstudents.com>
2024-11-15 22:21:17 +0100

    deleted range from final return

--- ES6_classes/100-evcar.js
+++ ES6_classes/100-evcar.js
@@ -1,18 +1,18 @@
 import Car from './10-car';
 
 export default class EVCar extends Car {
-    constructor(brand, motor, color, range) {
-        super(brand, motor, color);
-        this._range = range;
-    }
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
 
-    //range get method
-    get range() {
-        return this._range;
-    }
+  // range get method
+  get range() {
+    return this._range;
+  }
 
-    // new cloneCar with range added
-    cloneCar() {
-        return new Car(this._brand, this._motor, this._color, this._range);
-    }
+  // new cloneCar with range added
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
 }
