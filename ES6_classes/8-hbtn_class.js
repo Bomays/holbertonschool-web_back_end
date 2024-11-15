edf6d799de1b5c1a17fd21e81b2419712c6728b9
Bomays <9314@holbertonstudents.com>
2024-11-15 15:24:13 +0100

    Added HolbertonClass clss with size and location as attributes and defined a casting method for number and string types - eslint verified

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,1 +1,22 @@
-#!/usr/bin/python3
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  /* prefered Symbol.toPrimitive single method than valueOf()
+  and toString() for a better conversion control */
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+
+    if (hint === 'string') {
+      return this._location;
+    }
+
+    return this;
+    // return object itself for other types
+  }
+}
