68b6cf8e8fd63e08015ed4e38e30b3791d97e2c8
Bomays <9314@holbertonstudents.com>
2024-11-15 10:53:01 +0100

    Added Currency Class with name and code attributes private stored with their getter and setter before being used in a displayFullCurrency method that returns them in a string

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,39 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  //getter code
+  get code() {
+    return this._code;
+  }
+
+  //setter code
+  set code(value) {
+    if (typeof value === "string") {
+      this._code = value;
+    } else {
+      throw new TypeError("Code must be a string");
+    }
+  }
+
+  //getter name
+  get name() {
+    return this._name;
+  }
+
+  //setter name
+  set name(value) {
+    if (typeof value === "string") {
+      this._name = value;
+    } else {
+      throw new TypeError("Name must be a string");
+    }
+  }
+
+  //displayFullCurrency method
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
