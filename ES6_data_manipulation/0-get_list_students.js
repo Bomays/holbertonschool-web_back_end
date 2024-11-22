cd2750562a26b02069a6f970d248fb31dc92744e
Bomays <9314@holbertonstudents.com>
2024-11-22 10:53:29 +0100

    Added function getListStudents  returning an array of objects id (Number), firstName (String), and location (String)

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+export default function functiongetListStudents() {
+  const id = 1;
+  const firstName = 'Guillaume';
+  const location = 'San Francisco';
+  return [
+    { id, firstName, location },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
