9a1de0c3492729075ef39b4ffee703f79e68f091
Bomays <9314@holbertonstudents.com>
2024-11-25 17:56:52 +0100

    Added function handling multiple successful promises (used a promise chaining but vscode proposed me to convert it in an asynchrone function with a try - await - catch, see comment)

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,24 @@
+import {uploadPhoto, createUser} from './utils';
+
+
+export default function handleProfileSignup() {
+  const promise = Promise.all([uploadPhoto(), createUser()]);
+
+  return promise
+    .then((value) => console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`))
+    .catch(() => console.log('Signup system offline'));
+}
+
+/* vscode proposing :
+
+import { uploadPhoto, createUser } from './utils';
+
+export default async function handleProfileSignup() {
+  try {
+    const value = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+  } catch (error) {
+    console.log('Signup system offline');
+  }
+}
+*/
