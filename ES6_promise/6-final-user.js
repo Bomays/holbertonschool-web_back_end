1cd913330d85edb88cb8d8e8e7325fcea9b6d708
Bomays <9314@holbertonstudents.com>
2024-11-26 12:24:26 +0100

    eslint checked and fixed

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,19 +2,19 @@ import uploadPhoto from './5-photo-reject';
 import signUpuser from './4-user-promise';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-    const userPromise = signUpuser(firstName, lastName);
-    const photoPromise = uploadPhoto(fileName);
-    
-    try {
-        return await Promise.allSettled([userPromise, photoPromise])
-            .then((outputs) => outputs.map((output) => ({
-                status: output.status,
-                value: output.status === 'fulfilled' ? output.value : output.reason,
-            })));
-    } catch (error) {
-        return [
-            { status: 'rejected', value: error.toString() },
-            { status: 'rejected', value: error.toString() }
-        ];
-    }  
+  const userPromise = signUpuser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  try {
+    return await Promise.allSettled([userPromise, photoPromise])
+      .then((outputs) => outputs.map((output) => ({
+        status: output.status,
+        value: output.status === 'fulfilled' ? output.value : output.reason,
+      })));
+  } catch (error) {
+    return [
+      { status: 'rejected', value: error.toString() },
+      { status: 'rejected', value: error.toString() },
+    ];
+  }
 }
