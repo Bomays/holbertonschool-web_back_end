e60185fbd34bfcdbe73d90ca8942d41adaced9fe
Bomays <9314@holbertonstudents.com>
2024-11-26 12:21:40 +0100

    added Try/catch implementation to handle error and returning them all the time if happens

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,17 +1,20 @@
 import uploadPhoto from './5-photo-reject';
 import signUpuser from './4-user-promise';
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  fileName,
-) {
-  const userPromise = signUpuser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([userPromise, photoPromise])
-    .then((outputs) => outputs.map((output) => ({
-      status: output.status,
-      value: output.status === 'fulfilled' ? output.value : output.reason,
-    })));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+    const userPromise = signUpuser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
+    
+    try {
+        return await Promise.allSettled([userPromise, photoPromise])
+            .then((outputs) => outputs.map((output) => ({
+                status: output.status,
+                value: output.status === 'fulfilled' ? output.value : output.reason,
+            })));
+    } catch (error) {
+        return [
+            { status: 'rejected', value: error.toString() },
+            { status: 'rejected', value: error.toString() }
+        ];
+    }  
 }
