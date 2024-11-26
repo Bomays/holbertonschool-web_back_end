44b377f92789c6acb00f4f8e14ec66d254365923
Bomays <9314@holbertonstudents.com>
2024-11-26 12:31:46 +0100

    Undo changes from try/catch to simple return with then, just added missing toString to reason so if an error occurs it is outputed as an error mesage

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,16 +5,9 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   const userPromise = signUpuser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
-  try {
-    return await Promise.allSettled([userPromise, photoPromise])
-      .then((outputs) => outputs.map((output) => ({
-        status: output.status,
-        value: output.status === 'fulfilled' ? output.value : output.reason,
-      })));
-  } catch (error) {
-    return [
-      { status: 'rejected', value: error.toString() },
-      { status: 'rejected', value: error.toString() },
-    ];
-  }
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((outputs) => outputs.map((output) => ({
+      status: output.status,
+      value: output.status === 'fulfilled' ? output.value : output.reason.toString(),
+    })));
 }
