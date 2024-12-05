d55e5bfa5120838f6ce605618e53c5454478a3e3
Bomays <9314@holbertonstudents.com>
2024-12-05 15:31:47 +0100

    Added Tuple module from typing

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,9 +2,9 @@
 """Module that returns a string and a square of an int or a float  """
 
 
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Function that returns a string and a square of an int or a float"""
     return (k, v**2)
