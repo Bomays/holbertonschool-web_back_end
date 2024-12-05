371f57854f45b9454d928f949533ec1ebcbf6593
Bomays <9314@holbertonstudents.com>
2024-12-05 14:01:10 +0100

    Added type-annotated func that returns a string representation of a float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""module that converts a float to a string representation of it """
+
+
+def to_str(n: float) -> str:
+    """function that returns a string representation of a float """
+    return str(n)
