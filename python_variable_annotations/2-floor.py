5d08346bc48efb5401747fd184c8f29e055348a3
Bomays <9314@holbertonstudents.com>
2024-12-05 13:49:40 +0100

    Added type-annotated function that rounds down a float to the nearest integer as math.floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""module that rounds down a float to the nearest integer """
+
+
+def floor(n: float) -> float:
+    """function that returns a rounded integer from a float """
+    return int(n)
