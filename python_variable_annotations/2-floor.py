a24cb182e8db5c9d156ea1833dcd4664d0b4aa1f
Bomays <9314@holbertonstudents.com>
2024-12-05 13:54:51 +0100

    Corrected function type annotation

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -2,6 +2,6 @@
 """module that rounds down a float to the nearest integer """
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """function that returns a rounded integer from a float """
     return int(n)
