6d3af9336ab30b58df05e8a1b1a3480d13929a4d
Bomays <9314@holbertonstudents.com>
2024-12-05 16:45:03 +0100

    Annotated an element_length function parameters and return values with appropriate type: function that take either a list or an iterable of sequences and return a list of tuples where we find a sequence of the inputed list and its length

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Duck typing an iterable object
+> Annotate a function parameters and
+return values with appropriate type
+"""
+
+from typing import Iterable, List, Tuple, Sequence
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    function that take either a list or an iterable of
+    sequences and return a list of tuples where we find a sequence of
+    the inputed list and its length
+    """
+    return [(i, len(i)) for i in lst]
