3196983fddf679cac2c15d004ef6e248b240f9e9
Bomays <9314@holbertonstudents.com>
2024-12-06 13:59:12 +0100

    Corrected pycodestyle and added doc to inner async func

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -11,11 +11,13 @@ wait_n = __import__("1-concurrent_coroutines").wait_n
 
 def measure_time(n: int, max_delay: int) -> float:
     """
-    sync Function that returns the result of an async func with asyncio running:
+    sync Function that returns the result of an async func
+    with asyncio running:
     asyc func measures the time of the total execution
-    for wait_n and returns total_time / n  
+    for wait_n and returns total_time / n
     """
     async def async_time_measure():
+        """ async func to measure total time"""
         start_time = time.time()
         await wait_n(n, max_delay)
         end_time = time.time()
@@ -24,5 +26,6 @@ def measure_time(n: int, max_delay: int) -> float:
 
     return asyncio.run(async_time_measure())
 
-# to correct errors without changing main, had to pass a async func
-# inside the synch main func and returning it wth asyncio.run to execute it
+# to correct errors without changing main,
+# had to pass a async func inside the synch
+# main func and returning it wth asyncio.run to execute it
