74cec593cbf8644610f879f46f8a466c77c228bc
Bomays <9314@holbertonstudents.com>
2024-12-09 12:57:39 +0100

    Added Asyncgenerator from typing module in annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 """
 Module that handles a coroutine with a prototype
-with no arguments.
+with no arguments : async comprehesion
 """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
-    async Function that executes loops 10 times, each time
+    async Generator function that executes loops 10 times, each time
     asynchronously wait 1 second, then yield a random
     number between 0 and 10 using random module from asyncio
     """
