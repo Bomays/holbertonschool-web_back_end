115aca7e6ddb5ba374cccde87b7dc691bd0ac7d5
Bomays <9314@holbertonstudents.com>
2024-12-10 19:33:57 +0100

    Added implementation for get_hyper_index() > output tested, pycodestyled, and patched MYPY errors as: None type comparison with an int / get() method with a potential None key / optional type errors / index proper validation / type-annotation for data : List[] and removed unaccessed "truncated_dataset = dataset[:1000]"

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+from typing import Dict, List, Optional
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0"""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+                }
+        return self.__indexed_dataset
+
+    def get_hyper_index(
+            self, index: Optional[int] = None, page_size: int = 10
+            ) -> Dict:
+        """
+        Function that retrieves data  in a deletion-resilient pagination
+        Args:
+            index(int): starting index, must not be out of range of
+            indexed dataset
+            page_size(int): num of items in a page, by default 10
+
+        Raise:
+            AssertionError:
+                If the index provide is not an integer or is out of range
+
+        Returns:
+            Dict: a dictionnary with key/value pairs
+            {index, next_index, page_size, data}
+        """
+
+        index_dataset = self.indexed_dataset()
+
+        assert isinstance(index, int) and 0 <= index < len(index_dataset),\
+            "index must be in in the indexed dataset range"
+
+        data: list[list[str]] = []
+        current_index = index
+
+        while len(data) < page_size and current_index < len(index_dataset):
+            if index_dataset.get(current_index) is not None:
+
+                data.append(index_dataset[current_index])
+
+            current_index += 1
+
+        next_index: Optional[int] = current_index
+
+        while next_index is not None and next_index < len(index_dataset):
+            if index_dataset.get(next_index) is not None:
+                break
+
+            next_index += 1
+
+            if next_index is not None and next_index >= len(index_dataset):
+                next_index = None
+
+        return {
+            "index": index,
+            "next_index": next_index,
+            "page_size": len(data),
+            "data": data,
+        }
