7cf5818bfc414322c479780bf48f0a037f321469
Bomays <9314@holbertonstudents.com>
2024-12-13 22:08:43 +0100

    Updated and corrected client to db in defing nginx_collection

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,12 +5,12 @@ Python module that provides some stats about Nginx logs stored in MongoDB
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
-    """Stats about Nginx logs stored in MongoDB"""
+def nginx_stats():
+    """Function that provides Stats about Nginx logs stored in MongoDB"""
 
     client = MongoClient("mongodb://127.0.0.1:27017")
     db = client.logs
-    nginx_collection = client.nginx
+    nginx_collection = db.nginx
 
     n_logs = nginx_collection.count_documents({})
     print(f"{n_logs} logs")
@@ -27,3 +27,6 @@ if __name__ == "__main__":
         )
 
     print(f"{status_check} status check")
+
+if __name__ == "__main__":
+    nginx_stats()
