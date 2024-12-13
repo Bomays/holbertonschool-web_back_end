a268be5646caca5e04ca3242a9908cb9e7d4b281
Bomays <9314@holbertonstudents.com>
2024-12-13 21:59:34 +0100

    Updated db and collection and added forgotten PUT method

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -9,20 +9,21 @@ if __name__ == "__main__":
     """Stats about Nginx logs stored in MongoDB"""
 
     client = MongoClient("mongodb://127.0.0.1:27017")
-    nginx_collection = client.logs.nginx
+    db = client.logs
+    nginx_collection = client.nginx
 
     n_logs = nginx_collection.count_documents({})
     print(f"{n_logs} logs")
 
-    methods = ["GET", "POST", "PATCH", "DELETE"]
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
 
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
-        print(f"\tmethod{method}: {count}")
+        print(f"\tmethod {method}: {count}")
 
-    status_check = nginx_collection.count_documents({
-        "method": "GET", "path": "/status"
-        })
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+        )
 
     print(f"{status_check} status check")
