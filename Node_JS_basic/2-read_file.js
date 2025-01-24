b37853d9acf252b3716c8fcb5cc1513f4acf39f9
Bomays <nicolas.martinez@holbertonstudents.com>
2025-01-24 15:09:05 +0100

    updated const students with line filter trim

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -6,7 +6,7 @@ function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.split('\n').filter((line) => line.trim() !== '');
-    const students = lines.slice(1);
+    const students = lines.slice(1).filter((line) => line.trim() !== '');
 
     const fields = {};
     students.forEach((line) => {
@@ -22,11 +22,10 @@ function countStudents(path) {
     console.log(`Number of students: ${students.length}`);
 
     for (const field in fields) {
-      if (Object.prototype.hasOwnProperty.call(fields, field)) {
-        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-      }
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
     }
   } catch (error) {
+    console.error(error.message);
     throw new Error('Cannot load the database');
   }
 }
