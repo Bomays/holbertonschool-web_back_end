f7fcfc1d60043ee4d58d3e748865de711fc5969f
Bomays <nicolas.martinez@holbertonstudents.com>
2025-01-24 17:57:43 +0100

    updt

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,10 +2,10 @@
 
 const http = require('http');
 
-const app = http.createServer((_req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end("Hello Holberton School!");
+  res.end('Hello Holberton School!');
 });
 
 app.listen(1245);
