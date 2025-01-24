ea5301e7e3c6b413d77a2c70628448e4e814e161
Bomays <nicolas.martinez@holbertonstudents.com>
2025-01-24 16:45:46 +0100

    Updated all to go origin

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -4,40 +4,29 @@ const fs = require('fs'); // filesystem
 
 function countStudents(path) {
   try {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        console.error('Cannot load the database');
-        return;
-      }
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-      if (lines.length <= 1) {
-        console.error('Cannot load the database');
-        return;
-      }
-      const students = lines.slice(1);
-      const fields = {};
+    const data = fs.readFile(path, 'utf8');
+    const students = data.split('\n').filter((line) => line.trim() !== '');
 
-      students.forEach((line) => {
-        const parts = line.split(',');
-        if (parts.length >= 4) {
-          const [firstname, , , field] = parts;
-          if (firstname && field) {
-            if (!fields[field]) {
-              fields[field] = [];
-            }
-            fields[field].push(firstname);
-          }
+    const fields = {};
+
+    students.forEach((student) => {
+      const [firstname, , , field] = student.split(',');
+      if (firstname && field) {
+        if (!fields[field]) {
+          fields[field] = [];
         }
-      });
-
-      console.log(`Number of students: ${students.length}`);
-
-      for (const field in fields) {
-        if (Object.prototype.hasOwnProperty.call(fields, field)) {
-          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-        }
+        fields[field].push(firstname);
       }
     });
+
+    console.log(`Number of students: ${students.length}`);
+
+    for (const field in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List:`);
+        console.log(fields[field].join(', '));
+      }
+    }
   } catch (error) {
     throw new Error('Cannot load the database');
   }
