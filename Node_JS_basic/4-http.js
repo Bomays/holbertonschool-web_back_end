991d380ec3eb148ab94030c4d299885f44f0d5fc
Bomays <nicolas.martinez@holbertonstudents.com>
2025-01-24 17:29:28 +0100

    Added task 4 for creating simple HTTP server with Node for express (cf app. export)

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+// Script to create a simple HTTP server
+
+import { createServer } from 'http';
+
+const app = createServer((_req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
