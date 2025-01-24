ce6bd11cedd80dd4ba64abae4b89f787b1d682e3
Bomays <nicolas.martinez@holbertonstudents.com>
2025-01-24 21:46:57 +0100

    Updated

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -2,13 +2,13 @@
 
 const fs = require('fs'); // filesystem
 
-function countStudents(path) {
+async function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
       } else {
-        const students = data.split('\n').filter((line) => line.trim() !== '');
+        const students = data.split('\n').filter((line, index) => line.trim() !== '' && index > 0);
         const fields = {};
         students.forEach((student) => {
           const [firstname, , , field] = student.split(',');
@@ -20,13 +20,10 @@ function countStudents(path) {
 
         console.log(`Number of students: ${students.length}`);
 
-        for (const field in fields) {
-          if (Object.prototype.hasOwnProperty.call(fields, field)) {
-            console.log(`Number of students in ${field}: ${fields[field].length}. List:`);
-            console.log(fields[field].join(', '));
-          }
-        }
-        resolve();
+        Object.keys(fields).forEach((field) => {
+          console.log(`Number of students in ${field}: ${fields[field].length}. List: ` + fields[field].join(', '));
+        });
+        resolve(fields);
       }
     });
   });
