b97c6de77e3696a3e422f97342188996b6daf63c
Bomays <nicolas.martinez@holbertonstudents.com>
2025-01-24 07:15:12 +0100

    Replace console.log with process.stdout.write

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,16 @@
 // script making a prog displaying a message.
 
-console.log("Welcome to Holberton School, what is your name?");
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on("readable", function () {
   const name = process.stdin.read();
 
   if (name !== null) {
-    console.log(`Your name is: ${name.toString().trim()}`);
+    process.stdout.write(`Your name is: ${name.toString().trim()}\n`);
     //trim to ensure removing newline
   }
 });
 
 process.stdin.on("end", function () {
-  console.log("This important software is now closing");
+  process.stdout.write("This important software is now closing\n");
 });
