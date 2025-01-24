0425df1e8fc0940bcf35a52478feba63d5f9da67
Bomays <nicolas.martinez@holbertonstudents.com>
2025-01-24 13:44:06 +0100

    Uncorrect outputs: updated the student data parsing in my countStudents function to correctly split the CSV data into firstname, lastname, age, and field. And added "type": "module" to package.json to resolve warning.

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -5,21 +5,24 @@ import { readFileSync } from 'fs'; // filesystem node module
 function countStudents(path) {
   try {
     const data = readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');;
+    const students = lines.slice(1);
 
-    const students = data.split('\n').filter((line) => line !== '');
     const fields = {};
     students.forEach((student) => {
-      const [firstname, field] = student.split(',');
-      if (!fields[field]) {
+      const [firstname, lastname, age, field] = student.split(',');
+      if (field && field.trim()) {
+        if (!fields[field]) {
         fields[field] = [];
       }
       fields[field].push(firstname);
-    });
+    }
+  });
 
     console.log(`Number of students: ${students.length}`);
 
     for (const field in fields) {
-      if (field) {
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
         console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
       }
     }
