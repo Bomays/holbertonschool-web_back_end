9794b95faa09a5c38d726c31d63a7f0f5c4ce6f4
Bomays <nicolas.martinez@holbertonstudents.com>
2025-01-24 16:30:21 +0100

    Updated

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,43 +1,46 @@
 // Script that reads a file and prints the number of students and their names by field
 
-const fs = requires ('fs'); // filesystem node module
+const fs = require('fs'); // filesystem
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-    if (lines.length <= 1) {
-      console.error('Cannot load the database');
-      return;
-    }
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Cannot load the database');
+        return;
+      }
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      if (lines.length <= 1) {
+        console.error('Cannot load the database');
+        return;
+      }
+      const students = lines.slice(1);
+      const fields = {};
 
-    const students = lines.slice(1).filter((line) => line.trim() !== '');
-
-    const fields = {};
-    students.forEach((line) => {
-      const parts = line.split(',');
-      if (parts.length >= 4) {
-        const [firstname, , , field] = parts;
-        if (firstname && field) {
-          if (!fields[field]) {
-            fields[field] = [];
+      students.forEach((line) => {
+        const parts = line.split(',');
+        if (parts.length >= 4) {
+          const [firstname, , , field] = parts;
+          if (firstname && field) {
+            if (!fields[field]) {
+              fields[field] = [];
+            }
+            fields[field].push(firstname);
           }
-          fields[field].push(firstname);
+        }
+      });
+
+      console.log(`Number of students: ${students.length}`);
+
+      for (const field in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
         }
       }
     });
-
-    console.log(`Number of students: ${students.length}`);
-
-    for (const field in fields) {
-      if (Object.prototype.hasOwnProperty.call(fields, field)) {
-        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
-      }
-    }
   } catch (error) {
-    console.error(error.message);
     throw new Error('Cannot load the database');
   }
 }
 
-export default countStudents;
+module.exports = countStudents;
