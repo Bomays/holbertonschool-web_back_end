01f41ac8bd30d8ee974aaa1c0bc2738e23bc748a
Bomays <nicolas.martinez@holbertonstudents.com>
2025-01-24 20:39:01 +0100

    changed for more actual import export

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,8 +1,8 @@
 // Script to create a simple HTTP server
 
-const http = require('http');
+import { createServer } from 'http';
 
-const app = http.createServer((req, res) => {
+const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
@@ -10,4 +10,4 @@ const app = http.createServer((req, res) => {
 
 app.listen(1245);
 
-module.export = app;
+export default app;
