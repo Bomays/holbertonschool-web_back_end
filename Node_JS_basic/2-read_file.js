74fef2779720cf0163d56c169a856856cf9a76b2
Bomays <nicolas.martinez@holbertonstudents.com>
2025-01-24 13:50:05 +0100

    Undo variables because they are not used and eslint checked

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -5,19 +5,19 @@ import { readFileSync } from 'fs'; // filesystem node module
 function countStudents(path) {
   try {
     const data = readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter((line) => line.trim() !== '');;
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
     const students = lines.slice(1);
 
     const fields = {};
     students.forEach((student) => {
-      const [firstname, lastname, age, field] = student.split(',');
+      const [firstname, , , field] = student.split(',');
       if (field && field.trim()) {
         if (!fields[field]) {
-        fields[field] = [];
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
       }
-      fields[field].push(firstname);
-    }
-  });
+    });
 
     console.log(`Number of students: ${students.length}`);
 
